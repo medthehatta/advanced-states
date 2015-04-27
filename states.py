510e3f2e77200791221bdc6d3145cca0a18db50c
Med Mahmoud <mmahmoud@riverbed.com>
2015-04-26 22:47:04 -0700

    Initial commit of states class skeletons

---
+++ states.py
@@ -0,0 +1,22 @@
+# coding: utf8
+
+class Querier(object):
+    pass
+
+class State(object):
+    pass
+
+class FundamentalState(State):
+    pass
+
+class CompositeState(State):
+    pass
+
+class StateNegation(CompositeState):
+    pass
+
+class StateDisjunction(CompositeState):
+    pass
+
+class StateConjunction(CompositeState):
+    pass
