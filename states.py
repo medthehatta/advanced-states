0efde6a63ecc0b29faeb2d364e9fe57af276f4fa
Med Mahmoud <mmahmoud@riverbed.com>
2015-04-26 23:06:30 -0700

    Implement State

--- states.py
+++ states.py
@@ -1,5 +1,8 @@
 # coding: utf8
 
+from functools import lru_cache
+import re
+
 
 class Querier(object):
     def __init__(self, method, name='Unnamed Querier'):
@@ -26,14 +29,55 @@ class Querier(object):
         self.cache = None
 
     def __str__(self):
-        return "{0}:\n{1}".format(self.name(), self.query())
+        return '{0}:\n{1}'.format(self.name, self.query())
 
     def __repr__(self):
-        return "<Querier '{0}'>".format(self.name())
+        return '<Querier "{0}">'.format(self.name)
 
 
 class State(object):
-    pass
+    def __init__(self, *params, **kwargs):
+        raise NotImplementedError('Override me')
+
+    @classmethod
+    def get_canonical_name(name):
+        return re.sub(
+            r'\ +',
+            '_',
+            re.sub(
+                r'[^\w\ ]+',
+                '',
+                name.lower()
+            ),
+        )
+
+    @property
+    def name(self):
+        return self.name
+
+    @lru_cache
+    def canonical_name(self):
+        return State.get_canonical_name(self.name)
+
+    def inspect(self, *params, use_cache=1, **kwargs):
+        if not use_cache:
+            self.clear_caches()
+        return self._inspect(*params, **kwargs)
+
+    def _inspect(self, *params, **kwargs):
+        raise NotImplementedError('Override me')
+
+    @property
+    def children(self):
+        raise NotImplementedError('Override me')
+
+    @property
+    def queriers(self):
+        raise NotImplementedError('Override me')
+
+    def clear_caches(self):
+        for querier in self.queriers:
+            querier.clear()
 
 
 class FundamentalState(State):
